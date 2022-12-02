a4ad47c449af6cd4678e0fdce0e527b7ad6ce8ec
Lutfi Rahman <lutfi731>
2022-12-02 14:37:08 +0700

    finish no 1 and 2

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+function getRupiah($nominal){
+    $result_rupiah = "Rp. " . number_format($nominal, 0,'.','.');
+    return $result_rupiah;
+}
+
+function getUniqueCode($payment_method){
+    if ($payment_method == "Debit") {
+        $keyMethod = "DEB";
+    } else if ($payment_method == "Credit") {
+        $keyMethod = "CRE";
+    } else {
+        return "Unknown payment method!!!";
+    }
+    $keyDDMMYY = date("dmY");
+    $keyRandomNumber = rand(1000,9999);
+    return $keyMethod . $keyDDMMYY . $keyRandomNumber;
+}
+
+// show nominal
+// echo getRupiah(100000);
+
+// echo '<br>';
+
+// show unique code
+// echo getUniqueCode("Credit");
+?>
